4d26173fb6b9d19aa3cdd4e4ca95eb1902dfcdbb
Guanhua <william.ting.1116@gmail.com>
2019-03-24 22:33:18 +1000

    fixed function

--- Assets/NumberWizzard.cs
+++ Assets/NumberWizzard.cs
@@ -37,7 +37,7 @@ public class NumberWizzard : MonoBehaviour
         min = 1;
         guess = 1001 / 2;
 
-        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Welcome to number wizard!!!");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max + ".");
         Debug.Log("The lowest number you can pick is: " + min + ".");
